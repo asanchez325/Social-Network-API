b0ceca8fcb71c0b490617899526bf81b150d383b
Imperial Design <amsanchez@knox.edu>
2020-12-30 05:52:29 -0600

    initial file set-up

---
+++ controllers/user-controllers.js
@@ -0,0 +1,9 @@
+//GET all users
+
+//GET single user by id
+
+//POST new user
+
+//PUT to update user by id
+
+//DELETE to remove user by id
---
+++ routes/api/thoughts-routes.js
@@ -0,0 +1,11 @@
+//GET all thoughts
+
+//GET single thought by id
+
+//POST to create new thought
+
+//PUT to update a thought by its id
+
+//DELETE to delete thought by its id
+
+
